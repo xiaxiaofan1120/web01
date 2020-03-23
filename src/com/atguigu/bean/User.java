cd46095903b63a8ed7f1bf5dfb30ba736cbb7135
夏凡 <1794442741@qq.com>
2020-03-23 13:45:02 +0800

    Initial commit

---
+++ src/com/atguigu/bean/User.java
@@ -0,0 +1,12 @@
+package com.atguigu.bean;
+
+/**
+ * @author Administrator
+ * @create 2020-03-23 13:36
+ */
+public class User {
+
+    public static void main(String[] args) {
+        System.out.printf("hello");
+    }
+}
